cf5f9a2e72d53f1e498db4cd43b5ffaf36e68559
JstD <dung.truong2000@hcmut.edu.vn>
2020-12-28 16:44:48 +0700

    ..

--- LayerDAO.py
+++ LayerDAO.py
@@ -2,16 +2,16 @@
 from Model import Word
 import mysql.connector
 from _datetime import date
-def getConnection():
-    # tạo đối tượng connection
-    myconn = mysql.connector.connect(host = "127.0.0.1", user = "root",
-        password = "123456", database = "NNLTTT")
-    return myconn
 # def getConnection():
 #     # tạo đối tượng connection
-#     myconn = mysql.connector.connect(host = "127.0.0.1", user = "jstD",
-#         password = "Dung_2000", database = "NNLTTT")
+#     myconn = mysql.connector.connect(host = "127.0.0.1", user = "root",
+#         password = "123456", database = "NNLTTT")
 #     return myconn
+def getConnection():
+    # tạo đối tượng connection
+    myconn = mysql.connector.connect(host = "127.0.0.1", user = "jstD",
+        password = "Dung_2000", database = "NNLTTT")
+    return myconn
                                                                
 def findAll(sql):
     # tạo đối tượng cursor
@@ -158,3 +158,12 @@ def countNumOfBox(level):
     cur.execute(sql,par)
     myresult = cur.fetchone()
     return myresult[0]
+
+def totalResult():
+    myconn=getConnection()
+    cur = myconn.cursor()
+    sql = "select * from review"
+    cur.execute(sql)
+    myresult = cur.fetchall()
+    print(myresult)
+totalResult()
